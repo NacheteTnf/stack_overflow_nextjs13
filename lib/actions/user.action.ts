62d8af26ac3bc3d1030711b9d583fc1fd251b3f9
Nachete <ignaciocabrerahdez97@gmail.com>
2024-11-10 13:54:10 +0000

    routes, weebhook, user action adds.

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,6 +2,9 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose"
+import { CreateUserParams, DeleteUserParams, UpdateUserParams } from "./shared.types";
+import { revalidatePath } from "next/cache";
+import Question from "@/database/question.model";
 
 export async function getUserById(params:any) {
     try {
@@ -15,5 +18,69 @@ export async function getUserById(params:any) {
     } catch (error) {
         console.log(error);
         throw error;
-    }
 }
+
+export async function createUser(userData: CreateUserParams) {
+    try {
+      connectToDatabase();
+  
+      const newUser = await User.create(userData);
+      return newUser;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+  
+  export async function updateUser(params: UpdateUserParams) {
+    try {
+      connectToDatabase();
+  
+      const { clerkId, updateData, path } = params;
+  
+      await User.findOneAndUpdate({ clerkId }, updateData, {
+        new: true,
+      });
+  
+      revalidatePath(path);
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+  
+  export async function deleteUser(params: DeleteUserParams) {
+    try {
+      connectToDatabase();
+  
+      const { clerkId } = params;
+  
+      const user = await User.findOneAndDelete({ clerkId });
+      if (!user) {
+        throw new Error("User not found");
+      }
+      //  delete user from database
+      // and question, answer, comment from database
+  
+      // get user id from user
+       const userQuestionIds = await Question.find({ author: user._id }).distinct(
+         "_id"
+       );
+  
+      // delete question from database
+      // @ts-ignore
+      await Question.deleteMany({ author: user._id });
+  
+      // TODO: delete answer, answer from database
+  
+      // @ts-ignore
+      const deletedUser = await User.findByIdAndDelete(user._id);
+       return deletedUser;
+  
+      // finish delete user from database
+      //
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
