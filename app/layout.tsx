af5fe716b0b332510131cc47f91693066f5581b9
Nachete <ignaciocabrerahdez97@gmail.com>
2024-11-10 14:00:35 +0000

    CamelCase file fixed eslint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
+/* eslint-disable camelcase */
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 
-// eslint-disable-next-line camelcase
 import { Inter, Space_Grotesk } from "next/font/google";
 
 import { ThemeProvider } from "@/context/ThemeProvider";
